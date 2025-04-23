a9df34d11d4a88c3ac22402b11a1240deff6369e
stephaneBalelos <stephanedondyas2@gmail.com>
2025-04-24 00:00:47 +0200

    rebuild

--- dist/webflowcustompopup.js
+++ dist/webflowcustompopup.js
@@ -1,26 +1,27 @@
 function r() {
-  const e = document.getElementById("popup-modal"), s = e.querySelector("[close-popup]"), d = document.querySelector("[open-popup]");
+  const e = document.getElementById("popup-modal"), n = e.querySelector("[close-popup]"), s = document.querySelector("[open-popup]");
   let o = null;
-  const n = {
-    timeToTrigger: Math.abs(parseInt(e.getAttribute("time-to-trigger") || "0")) * 1e3,
-    closeButton: s
+  if (!e)
+    return;
+  const l = {
+    timeToTrigger: Math.abs(parseInt(e.getAttribute("time-to-trigger") || "0")) * 1e3
   }, i = () => {
     const t = document.activeElement;
     t && t.blur(), e.style.display = "none", e.setAttribute("aria-hidden", "true"), e.setAttribute("tabindex", "-1"), e.setAttribute("aria-modal", "false"), document.body.style.overflow = "auto", o && (clearTimeout(o), o = null);
-  }, l = () => {
+  }, d = () => {
     e.style.display = "flex", e.classList.add("show"), e.setAttribute("aria-hidden", "false"), e.setAttribute("tabindex", "0"), document.body.style.overflow = "hidden";
   };
   e.addEventListener("click", (t) => {
     t.target === e && i();
-  }), s.addEventListener("click", (t) => {
+  }), n && n.addEventListener("click", (t) => {
     t.preventDefault(), i();
-  }), d.addEventListener("click", (t) => {
-    t.preventDefault(), l();
-  }), n.timeToTrigger > 0 && (o = setTimeout(() => {
-    l();
-  }, n.timeToTrigger)), document.addEventListener("keydown", (t) => {
+  }), s && s.addEventListener("click", (t) => {
+    t.preventDefault(), d();
+  }), l.timeToTrigger > 0 && (o = setTimeout(() => {
+    d();
+  }, l.timeToTrigger)), document.addEventListener("keydown", (t) => {
     t.key === "Escape" && e.style.display === "flex" && i();
-  }), console.log(n);
+  });
 }
 export {
   r as setupPopupModal
